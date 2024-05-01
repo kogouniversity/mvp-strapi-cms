093c871a6bbb12b27bde1165637fd2d093c55e2e
scott0929 <scott0929@gmail.com>
2024-04-30 20:00:01 -0700

    dsd

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -6,14 +6,13 @@ import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::post.post', ({ strapi }) => ({
     async create(ctx) {
-        console.log('ctx: ', ctx);
-        console.log('ctx.state: ', ctx.state);
-
         // check if ctx.request.body has group
         // check if the group is one of ctx.state.user's groups
-
+        console.log(ctx.state.user);
         const newPost = await strapi.service('api::post.post').create(ctx);
+        console.log(newPost);
         const sanitizedPost = await this.sanitizeOutput(newPost, ctx);
+        console.log(sanitizedPost);
         ctx.body = sanitizedPost;
     },
 }));
