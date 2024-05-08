eb6bf17405c47e1908cdaee6c21ab60ad712a40f
scott0929 <scott0929@gmail.com>
2024-05-08 00:27:12 -0700

    block users done

--- src/api/__tests__/post/controllers/create.test.ts
+++ src/api/__tests__/post/controllers/create.test.ts
@@ -1,22 +1,21 @@
-import Strapi from '@strapi/strapi';
 import postController from '../../../post/controllers/post';
 
-jest.mock('@strapi/strapi', () => {
-    return {
+jest.mock('@strapi/strapi', () => ({
         factories: {
-            createCoreController: jest.fn((type, extend) => extend({
-                strapi: {
-                    entityService: {
-                        findMany: jest.fn(),
+            createCoreController: jest.fn((type, extend) =>
+                extend({
+                    strapi: {
+                        entityService: {
+                            findMany: jest.fn(),
+                        },
+                        query: jest.fn(() => ({
+                            create: jest.fn(),
+                        })),
                     },
-                    query: jest.fn(() => ({
-                        create: jest.fn(),
-                    })),
-                }
-            })),
-        }
-    };
-});
+                }),
+            ),
+        },
+    }));
 
 let ctx;
 let mockStrapi;
@@ -30,8 +29,8 @@ const mockUser = {
 
 const mockBodyWithGroup = {
     data: {
-        title: "Test post",
-        content: "Test content",
+        title: 'Test post',
+        content: 'Test content',
         group: { id: 1 },
     },
 };
@@ -39,17 +38,17 @@ const mockBodyWithGroup = {
 const userGroups = [
     {
         id: 1,
-        name: "Group One",
-        users: [{ id: 3 }]
+        name: 'Group One',
+        users: [{ id: 3 }],
     },
 ];
 
 const newPost = {
     id: 1,
-    title: "Test post",
-    content: "Test content",
+    title: 'Test post',
+    content: 'Test content',
     group: { id: 1 },
-}
+};
 
 describe('Post Controller Create Method', () => {
     beforeEach(() => {
@@ -68,19 +67,21 @@ describe('Post Controller Create Method', () => {
             },
             badRequest: jest.fn(),
             request: {
-                body: mockBodyWithGroup
+                body: mockBodyWithGroup,
             },
             send: jest.fn(),
         };
         mockStrapi.entityService.findMany.mockResolvedValue(userGroups);
     });
     it('should create a new post', async () => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         await (postController as any).create(ctx);
         ctx.send(newPost);
         expect(ctx.send).toHaveBeenCalledWith(newPost);
     });
     it('should return error if group is not provided', async () => {
         ctx.request.body.data.group = null;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         await (postController as any).create(ctx);
         expect(ctx.badRequest).toHaveBeenCalledWith('Group is required.', {
             errors: {
@@ -91,6 +92,7 @@ describe('Post Controller Create Method', () => {
     it('should return error if user is not a member of the provided group', async () => {
         mockStrapi.entityService.findMany.mockResolvedValue([]);
         ctx.request.body.data.group = { id: 1 };
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         await (postController as any).create(ctx);
         expect(ctx.badRequest).toHaveBeenCalledWith('User is not a member of the provided group.', {
             errors: {
--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -5,10 +5,10 @@
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async create(ctx: any) {
         const { user } = ctx.state;
         const { title, content, group } = ctx.request.body.data;
-        console.log(ctx.request.body.data)
 
         // check if ctx.request.body has group(the group must be provided in the request body)
         if (!group) {
@@ -21,10 +21,11 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
 
         // check if the group is one of ctx.state.user's groups(the user has to be in the group to create a post)
         // 1. find all the groups
-        const userGroups = await strapi.entityService.findMany('api::group.group', {
-            filters: { users: user.id },
-            populate: { users: true },
-        }) || [];
+        const userGroups =
+            (await strapi.entityService.findMany('api::group.group', {
+                filters: { users: user.id },
+                populate: { users: true },
+            })) || [];
 
         // 2. check if the group is in the user's groups
         const isUserInGroup = userGroups.find(g => g.id === group.id);
@@ -38,12 +39,12 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
 
         // 3. create a new post
         const newPost = await strapi.query('api::post.post').create({
-            data:{
+            data: {
                 title,
                 content,
                 group: group.id,
                 author: user.id,
-            }
+            },
         });
 
         return ctx.send(newPost);
