d6e64d19bc0e8d8952f61d87494099e68654c3a2
Kathy <jikathycho@gmail.com>
2024-06-26 21:07:31 -0700

    update likes

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -148,11 +148,11 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
         const { user } = ctx.state;
         const { postId } = ctx.params;
         // check if post'likes already have userid
-        const isLiked = await redis().sismember(`post-${postId}-user-${user.id}-like`, user.id);
+        const isLiked = await redis().sismember(`post-${postId}-like`, user.id);
 
         if (!isLiked) {
             // user haven't liked the post
-            await redis().sadd(`post-${postId}-user-${user.id}-like`, user.id);
+            await redis().sadd(`post-${postId}-like`, user.id);
             return user.id;
         }
         return ctx.badRequest('User already liked the post');
@@ -162,10 +162,10 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
         const { user } = ctx.state;
         const { postId } = ctx.params;
         // delete user from redis set of the post
-        const isLiked = await redis().sismember(`post-${postId}-user-${user.id}-like`, user.id);
+        const isLiked = await redis().sismember(`post-${postId}-like`, user.id);
 
         if (isLiked) {
-            await redis().srem(`post-${postId}-user-${user.id}-like`, user.id);
+            await redis().srem(`post-${postId}-like`, user.id);
             return user.id;
         }
         return ctx.badRequest("User didn't like this post before");
